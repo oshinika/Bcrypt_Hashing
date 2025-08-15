6d1e895e04c66d7006e0f949c060db963fbb825c
oshinika <oshinikar@gmail.com>
2025-08-15 17:50:33 +0530

    done

---
+++ components/org.wso2.carbon.identity.hash.provider.bcrypt/src/main/java/org/wso2/carbon/identity/hash/provider/bcrypt/internal/BcryptHashServiceComponent.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.hash.provider.bcrypt.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.wso2.carbon.identity.hash.provider.bcrypt.BcryptHashProviderFactory;
+import org.wso2.carbon.user.core.hash.HashProviderFactory;
+
+/**
+ * This class contains the PBKDF2 hashing service component.
+ */
+@Component(
+        name = "org.wso2.carbon.identity.hash.provider.pbkdf2.component",
+        immediate = true
+)
+public class BcryptHashServiceComponent {
+
+    private static final Log log = LogFactory.getLog(BcryptHashServiceComponent.class);
+
+    @Activate
+    protected void activate(ComponentContext ctxt) {
+
+        try {
+            HashProviderFactory hashProviderFactory = new BcryptHashProviderFactory();
+            ctxt.getBundleContext().registerService(HashProviderFactory.class.getName(),
+                    hashProviderFactory, null);
+
+            if (log.isDebugEnabled()) {
+                log.debug("PBKDF2 bundle activated successfully.");
+            }
+        } catch (Throwable e) {
+            log.error("Failed to activate PBKDF bundle", e);
+        }
+    }
+
+    @Deactivate
+    protected void deactivate(ComponentContext ctxt) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("PBKDF2 bundle is deactivated ");
+        }
+    }
+}
